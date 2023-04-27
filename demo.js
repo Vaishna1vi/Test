d473a1422846c1cdbbe3186f4c3fc0d674fba0d6
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2023-04-27 23:59:38 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,7 @@
 // console.log("hello");
 prompt("Enter your massage");
-alert("Hello");
+//alert("Hello");
 alert("Have a nice time!");
 const hi = () => {
-
-    document.getElementById("hi").innerHTML = "Hello";
+    document.getElementById("hi").innerHTML = "Hello! This is testing.";
 }
