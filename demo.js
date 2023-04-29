cb2798622a04f659beef9fe062b309d1db0b6e55
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-04-29 23:58:00 +0530

    little changes

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 // console.log("hello");
-prompt("Enter your massage");
-alert("Hello");
-alert("Have a nice time!");
+// prompt("Enter your massage");
+// alert("Hello");
+// alert("Have a nice time!");
 const hi = () => {
 
     document.getElementById("hi").innerHTML = "Hello";
